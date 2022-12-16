7adfdbf66a405377104067a8f2f4652bcdc2ffce
Hakan <hakanogretmen@gmail.com>
2022-12-16 16:46:40 +0300

    end

---
+++ WebApplication1/ActionFilters/ExceptionActionFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace WebApplication1.ActionFilters
+{
+	public class ExceptionActionFilter:ExceptionFilterAttribute
+	{
+		public override Task OnExceptionAsync(ExceptionContext context)
+		{
+			var controllerName = context.RouteData.Values["controller"];
+			var actionName = context.RouteData.Values["action"];
+			Debug.WriteLine("Exception Filter : " + controllerName + " - " + actionName);
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ WebApplication1/ActionFilters/LogActionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NuGet.Protocol.Plugins;
+using System.Diagnostics;
+
+namespace WebApplication1.ActionFilters
+{
+	public class LogActionFilter : ActionFilterAttribute
+	{
+		public override void OnActionExecuted(ActionExecutedContext context)
+		{
+			var controllerName = context.RouteData.Values["controller"];
+			var actionName = context.RouteData.Values["action"];
+
+			Debug.WriteLine("Action Log : " + controllerName+ " - " + actionName);
+		}
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var data = context.HttpContext.Session.GetString("userName");
+
+            if (data != null)
+            {
+                Controller controller = context.Controller as Controller;
+                controller.ViewBag.UserName = data;
+
+            }
+        }
+    }
+}
