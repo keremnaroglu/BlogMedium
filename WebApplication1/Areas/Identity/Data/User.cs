b6e22655583d8f102e4729fb880049274250d4c2
huseyinerdin <erdinh61@gmail.com>
2022-12-16 16:37:56 +0300

    aaa

---
+++ WebApplication1/Areas/Identity/Data/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+namespace WebApplication1.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the User class
+public class User : IdentityUser
+{
+    [PersonalData]
+    [EmailAddress]
+    [System.ComponentModel.DataAnnotations.Required]
+    public string Mail { get; set; }
+
+    [System.ComponentModel.DataAnnotations.Required]
+    [PersonalData]
+    public string Sifre { get; set; }
+}
+
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,20 @@
 using WebApplication1.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("WebApplication1ContextConnection") ?? throw new InvalidOperationException("Connection string 'WebApplication1ContextConnection' not found.");
+
+builder.Services.AddDbContext<WebApplication1Context>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<WebApplication1Context>();
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 DependencyInjection.AddScopeEf(builder.Services);
 
@@ -20,11 +32,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
